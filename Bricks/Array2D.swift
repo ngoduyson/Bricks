56bb03871d40a2ad15fe2d73229903461bb4ff10
framgia <framgia@framgia-45-no-MacBook-Pro.local>
2015-10-09 15:09:32 +0700

    Add array for screen controlling

--- Bricks/Array2D.swift
+++ Bricks/Array2D.swift
@@ -6,4 +6,27 @@
 //  Copyright © 2015年 NGO SON. All rights reserved.
 //
 
-import Foundation
+// #1
+class Array2D<T> {
+    let columns: Int
+    let rows: Int
+// #2
+    var array: Array<T?>
+    
+    init(columns: Int, rows: Int) {
+        self.columns = columns
+        self.rows = rows
+// #3
+        array = Array<T?>(count: rows * columns, repeatedValue: nil)
+    }
+    
+// #4
+    subscript(column: Int, row: Int) -> T? {
+        get {
+            return array[(row * columns) + column]
+        }
+        set(newValue) {
+            array[(row * columns) + column] = newValue
+        }
+    }
+}
